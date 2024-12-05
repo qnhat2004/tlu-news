421024620ce90fb46511f29bc32c9b289a2d6cc7
Vũ Quang Nhật <nhatchien12@gmail.com>
2024-12-05 14:55:03 +0700

    update database

--- models/Database.php
+++ models/Database.php
@@ -5,15 +5,17 @@ class Database {
         $dbname = 'news-reading';
         $username = 'root';
         $password = '';
-        $option = [
+        $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         ];
-        try {
-            $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, $option);
-        } catch (PDOException $e) {
-            echo ''. $e->getMessage();
-            exit();
-        }
+        // try {
+        //     $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, $option);
+        //     return $db;
+        // } catch (PDOException $e) {
+        //     echo ''. $e->getMessage();
+        //     return null;
+        // }
+        return new PDO("mysql:host=$host;dbname=$dbname", $username, $password, $options);
     }
 }
