428cacf7b79467fd4ef130f377bd17cd0aeabc25
Vũ Quang Nhật <nhatchien12@gmail.com>
2024-12-05 14:52:51 +0700

    update index

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require 'models/News.php';
+require 'controllers/NewsController.php';
+
+$controller = isset($_GET['controller']) ? $_GET['controller'] : 'news';
+$action = isset($_GET['action']) ? $_GET['action'] : 'index';
+$id = isset($_GET['id']) ? $_GET['id'] : null;
+
+switch ($controller) {
+    case 'news':
+        $controller = new NewsController();
+        break;
+}
+
+if ($id !== null) {
+    $controller->{$action}($id);
+} else {
+    $controller->{$action}();
+}
+?>
