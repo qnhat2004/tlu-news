514af2bdb7c10c58d0351541cdf2209cceccbef7
Vũ Quang Nhật <nhatchien12@gmail.com>
2024-12-07 01:40:04 +0700

    remove commented-out PDO connection code in Database model

--- models/Database.php
+++ models/Database.php
@@ -9,13 +9,6 @@ class Database {
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         ];
-        // try {
-        //     $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, $option);
-        //     return $db;
-        // } catch (PDOException $e) {
-        //     echo ''. $e->getMessage();
-        //     return null;
-        // }
         return new PDO("mysql:host=$host;dbname=$dbname", $username, $password, $options);
     }
 }
