962a420b3fce5b8883e61724877f796696535010
Vũ Quang Nhật <nhatchien12@gmail.com>
2024-12-07 01:39:35 +0700

    refactor news detail layout for improved styling and responsiveness

--- views/news/detail.php
+++ views/news/detail.php
@@ -1,25 +1,23 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header"><h3>
-<?php
-    echo $new->getTitle();
-?>
-
-                </h3></div>
-
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card shadow-sm">
+                <div class="card-header">
+                    <h3 class="mb-0">
+                        <?php echo $new->getTitle(); ?>
+                    </h3>
+                </div>
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-md-2">
-                            <img src="<?php echo $new->getImage(); ?>" class="img-fluid mb-4" alt="<?php echo $new->getTitle(); ?>">
+                        <div class="col-md-4">
+                            <img src="<?php echo $new->getImage(); ?>" class="img-fluid rounded mb-4" alt="<?php echo $new->getTitle(); ?>">
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-8">
                             <p><?php echo $new->getContent(); ?></p>
                         </div>
-                        <div class="text-muted">
-                            Đăng ngày: <?php echo $new->getCreatedAt(); ?>
-                        </div>
+                    </div>
+                    <div class="text-muted text-right">
+                        Đăng ngày: <?php echo $new->getCreatedAt(); ?>
                     </div>
                 </div>
             </div>
