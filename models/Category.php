497de2160e6c3971a021631b288b646a0764874d
Vũ Quang Nhật <nhatchien12@gmail.com>
2024-12-07 01:39:58 +0700

    add Category model with properties and methods for ID and name management

---
+++ models/Category.php
@@ -0,0 +1,18 @@
+<?php
+class Category {
+    private $id, $name;
+
+    public function __construct($id, $name) {
+        $this->id = $id;
+        $this->name = $name;
+    }
+    public function getId() {
+        return $this->id;
+    }
+    public function getName() {
+        return $this->name;
+    }
+    public function setName($name) {
+        $this->name = $name;
+    }
+}
