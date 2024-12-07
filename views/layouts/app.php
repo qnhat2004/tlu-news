7eef52acd6923bcb6635cc115b9423297a1ccb05
Vũ Quang Nhật <nhatchien12@gmail.com>
2024-12-07 11:23:03 +0700

    fixed display user or login logout

--- views/layouts/app.php
+++ views/layouts/app.php
@@ -16,7 +16,8 @@
 
     <!-- Style and Link CDN -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     <!-- Bao gồm jQuery và Bootstrap JS -->
@@ -25,6 +26,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
@@ -46,15 +48,28 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <form class="form-inline mb-4 mr-2" method="GET" action="index.php?controller=home">
-                            <input class="form-control mr-2" type="search" name="search_input" placeholder="Tìm kiếm tin tức">
+                            <input class="form-control mr-2" type="search" name="search_input"
+                                placeholder="Tìm kiếm tin tức">
                             <button class="btn btn-success" type="submit">Tìm kiếm</button>
                         </form>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php?controller=auth&action=showLoginForm">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php?controller=auth&action=showRegisterForm">Register</a>
-                        </li>
+                        <?php if (!isset($_SESSION['name'])): ?>
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php?controller=auth&action=showLoginForm">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php?controller=auth&action=showRegisterForm">Register</a>
+                            </li>
+                        <?php else: ?>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <?php echo $_SESSION['name']; ?>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="index.php?controller=auth&action=logout">Logout</a>
+                                </div>
+                            </li>
+                        <?php endif; ?>
                     </ul>
                 </div>
             </div>
@@ -62,10 +77,10 @@
     </div>
 
     <main class="py-4">
-        <?php 
+        <?php
         if (isset($view))
             include($view);
-        else 
+        else
             echo "view not found";
         ?>
     </main>
