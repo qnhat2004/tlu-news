659160a96a882f7aba4e29fad8809c9e413ce006
Vũ Quang Nhật <nhatchien12@gmail.com>
2024-12-07 11:22:40 +0700

    fixed login and register form

--- views/auth/login.php
+++ views/auth/login.php
@@ -7,18 +7,20 @@
                     <form method="POST" action="index.php?controller=auth&action=login">
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">Email Address</label>
-                            <div class="col-md-4 col-form-label text-md-end">
+                            <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" required
                                     autocomplete="email" autofocus>
                             </div>
                         </div>
+                        
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">Password</label>
-                            <div class="col-md-4 col-form-label text-md-end">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">Password</label>
+                            <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required
                                     autocomplete="current-password">
                             </div>
                         </div>
+
                         <div class="row mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
@@ -29,12 +31,13 @@
                                 </div>
                             </div>
                         </div>
+
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     Login
                                 </button>
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                <a class="btn btn-link" href="index.php?controller=auth&action=forgotPassword">
                                     Forgot Your Password?
                                 </a>
                             </div>
--- views/auth/register.php
+++ views/auth/register.php
@@ -4,7 +4,7 @@
             <div class="card">
                 <div class="card-header text-center">Register</div>
                 <div class="card-body">
-                    <form method="POST" action="index.php?controller=auth&action=login">
+                    <form method="POST" action="index.php?controller=auth&action=register">
                         <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
                             <div class="col-md-6">
@@ -12,54 +12,46 @@
                                     autocomplete="name" autofocus>
                             </div>
                         </div>
+
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">Email Address</label>
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" required
-                                    autocomplete="email" autofocus>
+                                    autocomplete="email">
                             </div>
                         </div>
+
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">Role</label>
-                            <div class="col-md-4 col-form-label text-md-end">
+                            <label for="role" class="col-md-4 col-form-label text-md-end">Role</label>
+                            <div class="col-md-6">
                                 <select name="role" id="role" class="form-control">
                                     <option value="admin">Admin</option>
                                     <option value="user">User</option>
                                 </select>
                             </div>
                         </div>
+
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">Password</label>
-                            <div class="col-md-4 col-form-label text-md-end">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">Password</label>
+                            <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required
-                                    autocomplete="current-password">
+                                    autocomplete="new-password">
                             </div>
                         </div>
+
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">Confirm Password</label>
-                            <div class="col-md-4 col-form-label text-md-end">
-                                <input id="password" type="password" class="form-control" name="password" required
-                                    autocomplete="current-password">
+                            <label for="password_confirm" class="col-md-4 col-form-label text-md-end">Confirm Password</label>
+                            <div class="col-md-6">
+                                <input id="password_confirm" type="password" class="form-control" name="password_confirm" required
+                                    autocomplete="new-password">
                             </div>
                         </div>
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember">
-                                    <label class="form-check-label" for="remember">
-                                        Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Login
+                                    Register
                                 </button>
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
                             </div>
                         </div>
                     </form>
