8810fd2fef8d3be490ad6b6efd5ef4bfb5529e13
Vũ Quang Nhật <nhatchien12@gmail.com>
2024-12-07 10:04:15 +0700

    add HomeController and AuthController to index.php for improved routing

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 require_once 'controllers/NewsController.php';
 require_once 'controllers/AdminController.php';
+require_once 'controllers/HomeController.php';
+require_once 'controllers/AuthController.php';
 
 $controller = isset($_GET['controller']) ? $_GET['controller'] : 'news';
 $action = isset($_GET['action']) ? $_GET['action'] : 'index';
 $id = isset($_GET['id']) ? $_GET['id'] : null;
+
+// if ($controller === 'admin' && !isAdmin()) {
+//     header('Location: index.php?controller=auth&action=login');
+//     exit();
+// }
 
 switch ($controller) {
     case 'news':
@@ -12,6 +19,12 @@ switch ($controller) {
         break;
     case 'admin':
         $controller = new AdminController();
+        break;
+    case 'home':
+        $controller = new HomeController();
+        break;
+    case 'auth':
+        $controller = new AuthController();
         break;
 }
 
