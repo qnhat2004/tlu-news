9e954d68cfa10a02ae57c3b84fd80b1e1f85b112
Vũ Quang Nhật <nhatchien12@gmail.com>
2024-12-07 11:21:57 +0700

    update authservice

--- services/AuthService.php
+++ services/AuthService.php
@@ -3,19 +3,17 @@ require_once 'models/Database.php';
 require_once 'models/User.php';
 class AuthService 
 {
-    public static function login($email, $password)
+    public function login($email, $password)
     {
         try {
             $db = Database::connect();
-            $sql = 'SELECT * FROM users WHERE email = :email AND password = :password';
+            $sql = 'SELECT * FROM users WHERE email = :email';
             $stmt = $db->prepare($sql);
-            $stmt->bindParam('$email', $username, PDO::PARAM_STR);
-            $stmt->bindParam('password', $email, PDO::PARAM_STR);
+            $stmt->bindParam(':email', $email, PDO::PARAM_STR);
             $stmt->execute();
-            $user = null;
-            while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                $user = new User($row['name'], $row['email'], $row['password'], $row['role']);
-                return $user;
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            if($row && password_verify($password, $row['password'])) {
+                return new User($row['name'], $row['email'], $row['password'], $row['role']);
             }
         } catch (PDOException $e) {
             echo '' . $e->getMessage();
@@ -23,19 +21,18 @@ class AuthService
         }
     }
 
-    public static function register($name, $email, $password, $role)
+    public function register($name, $email, $password, $role)
     {
         try {
             $db = Database::connect();
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
             $sql = 'INSERT INTO users (name, email, password, role) VALUES (:name, :email, :password, :role)';
             $stmt = $db->prepare($sql);
             $stmt->bindParam(':name', $name, PDO::PARAM_STR);
             $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-            $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+            $stmt->bindParam(':password', $hashedPassword, PDO::PARAM_STR);
             $stmt->bindParam(':role', $role, PDO::PARAM_STR);
-            $stmt->execute();
-            $user = new User($name, $email, $password, 'user');
-            return $user;
+            return $stmt->execute();
         } catch (PDOException $e) {
             echo '' . $e->getMessage();
             return null;
